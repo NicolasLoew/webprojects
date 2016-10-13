d9ebd3b147702193c53953436bd6b5cbcc78a862
Nicolas Loew <ympker@web.de>
2016-10-13 12:52:41 +0200

    Update
    Update credits

--- MyAnimeList/credits.js
+++ MyAnimeList/credits.js
@@ -1,4 +1,4 @@
 console.log("This is a WIP.")
-console.log("Credits: Ympker")
+console.log("Credits: Ympker/Damon")
 console.log("People I would like to thank: Cornex")
 console.log("This project is at GitHub: https://github.com/NicolasLoew/webprojects/tree/master/MyAnimeList")
